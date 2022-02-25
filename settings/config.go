252b804cea612584ef3114373568c39ae9c31a88
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-02-24 23:29:38 -0300

    Add multiple configuraiton settings

--- settings/config.go
+++ settings/config.go
@@ -33,8 +33,24 @@ func getParam(key string, defaultValue string) string {
 }
 
 func Setup() {
-	godotenv.Load()
+	println("Loading .env file...")
+	env := os.Getenv("TWITCH_GO_BACKEND_ENV")
+	println("Detected environment: " + env)
+	switch env {
+	case "devl":
+		godotenv.Load(".env.devl")
+		println("Loading .env.devl")
+		break
+	case "prod":
+		godotenv.Load(".env.prod")
+		println("Loading .env.prod")
+		break
+	default:
+		godotenv.Load()
+		println("Loading .env")
+	}
 
+	println("Loading settings...")
 	ServerSettings.Addr = getParam("ADDR", ":8080")
 	ServerSettings.ReadTimeout, _ = strconv.Atoi(getParam("READ_TIMEOUT", "10"))
 	ServerSettings.WriteTimeout, _ = strconv.Atoi(getParam("WRITE_TIMEOUT", "10"))
@@ -43,4 +59,5 @@ func Setup() {
 	ServerSettings.TwitchClientID = getParam("TWITCH_CLIENT_ID", "")
 	ServerSettings.TwitchClientSecret = getParam("TWITCH_CLIENT_SECRET", "")
 
+	println("Settings loaded!")
 }
