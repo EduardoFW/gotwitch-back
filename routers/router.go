9860df71eb8a3b859d49032065f4c9b0aca2ac87
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-02-24 22:53:13 -0300

    Add cors

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	v1 "github.com/EduardoWeber/twitch-go/controllers/v1"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 // InitRouter initialize routing information
@@ -10,6 +11,8 @@ func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	r.Use(cors.Default())
 
 	apiV1 := r.Group("/api/v1")
 	{
